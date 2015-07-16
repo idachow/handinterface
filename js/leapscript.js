097e789385e0cf3a8873bab2b8fe46cb2e38613c
Ida Chow <ida@Idas-MacBook-Pro.local>
2015-07-15 18:39:25 -0700

    swipe gesture

--- js/leapscript.js
+++ js/leapscript.js
@@ -8,21 +8,35 @@ Leap.loop(controllerOptions, function(frame) {
   if (frame.gestures.length > 0) {
     for (var i = 0; i < frame.gestures.length; i++) {
       var gesture = frame.gestures[i];
-      switch (gesture.type) {
-        case "circle":
-          // refresh screen
+      if(gesture.type == "circle") {
           console.log("circle recognized, refresh");
           window.location.reload(true);
           break;
-        case "swipe":
-          console.log("swipe recognized, link out");
-          window.open("linkto.html","_self");
-          break;
-        case "keyTap":
-          console.log("tap recognized, link back");
-          window.open("calendardemo.html", "_self");
+      }
+      if(gesture.type == "swipe") {
+          var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+
+          if(isHorizontal) {
+            if(gesture.direction[0] > 0){
+                  swipeDirection = "right";
+              } else {
+                  swipeDirection = "left";
+              }
+            console.log(swipeDirection + " --swipe recognized, link out");
+            if(swipeDirection == "right") {
+              window.open("calendardemo.html", "_self");
+            }
+            if (swipeDirection == "left") {
+              window.open("linkto.html", "_self");
+            }
+          }
           break;
       }
+      // if(gesture.type =="keyTap") {
+      //     console.log("tap recognized, link back");
+      //     window.open("calendardemo.html", "_self");
+      //     break;
+      // }
     }
   }
 
